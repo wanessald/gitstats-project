1cdd1cc0275ca02fc05e63a076dcf796bf7ccc77
wanessald <wanessaldantas@gmail.com>
2023-05-08 15:59:44 -0300

    Alteração teste

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+export function Home() {
+    return (
+        <div className="home">
+            <h1>GITSTATS</h1>
+            <div className="container">
+                <h1>Buscador de Perfis GitHub</h1>
+                <label>Pesquisa</label>
+
+            </div>
+        </div>
+    );
+}
