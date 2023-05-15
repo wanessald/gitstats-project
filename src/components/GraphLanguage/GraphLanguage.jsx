e2fc8c07b901bf6201f8ffb98591ac61e9af9e37
Paahxp123 <joaopedro2k2294@gmail.com>
2023-05-15 10:25:58 -0300

    alterações feitas no grafico

--- src/components/GraphLanguage/GraphLanguage.jsx
+++ src/components/GraphLanguage/GraphLanguage.jsx
@@ -3,6 +3,9 @@
 import React from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -33,11 +36,50 @@ export const data = {
   ],
 };
 
-export function GraphLanguage() {
+export function GraphLanguage(parametros) {
+  const [graphData, setGraphData] = useState(data);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.github.com/repos/${parametros.dono}/${parametros.repo}/languages`)
+      .then((response) => {
+        let ddata = {
+          labels: Object.keys(response.data),
+          datasets: [
+            {
+              label: '# of Votes',
+              data: Object.values(response.data),
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+        };
+        setGraphData(ddata);
+        
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [parametros.dono, parametros.repo]);
     return (
         <div style={{height: 300}}>
-          <h4>Linguagem mais usada</h4>
-          <Doughnut data={data} />
+          <h4>Linguagem mais usada {parametros.dono} {parametros.repo}</h4>
+          <Doughnut data={graphData} />
         </div>
     );
 }
