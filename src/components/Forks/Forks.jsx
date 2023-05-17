4fdaff967ee4db36b705181f013518e24b43e857
Williana123 <willianavictoria31@gmail.com>
2023-05-17 16:00:41 -0300

    Forks

---
+++ src/components/Forks/Forks.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+export function Forks({ dono, repo }) {
+  const [forkCount, setForkCount] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.github.com/repos/${dono}/${repo}/forks`)
+      .then((response) => {
+        setForkCount(response.data.length);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [dono, repo]);
+
+  return <p>Número de forks: {forkCount}</p>;
+}
