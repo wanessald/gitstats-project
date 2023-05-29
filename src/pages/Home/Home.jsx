f91959ce84a2ee8556011150f5dcdc009853bf27
Williana123 <willianavictoria31@gmail.com>
2023-05-29 13:55:07 -0300

    ok

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -40,79 +40,79 @@ export function Home() {
   const handleCloseModal = () => {
     setShowModal(false);
   };
+
   const toggleTheme = () => {
     setTheme(theme === 'light' ? 'dark' : 'light');
   };
 
   return (
-    <main className="principal">
-        <header className="d-flex align-items-center">
-        <div style={{ position: "relative", left: "88rem"}}>
+    <main className={`principal ${theme}`}>
+      <header className="d-flex align-items-center">
+        <div style={{ position: "relative", left: "88rem" }}>
           <ThemeToggle toggleTheme={toggleTheme} />
         </div>
       </header>
-        <Container>
+      <Container>
         <div className={`form ${theme}`}>
-            <Form>
-              <h1 style={{ fontFamily: "Karla" }}>GitStats</h1>
-              <h2>
-                Pesquise por perfis e acompanhe as estatísticas de seus usuários
-                e repositórios de maneira regular.
-              </h2>
-              <InputGroup className={`busca ${theme}`} style={{border:"none", borderRadius:"15px"}}>
-                <Form.Control
-                  type="text"
-                  className="input-name-user"
-                  placeholder="Digite o nome do usuário"
-                  onChange={handleUsernameSearch}
-                  onKeyDown={handleKeyDown}
-                  value={userName}
-                />
-                <Button
-                  variant="primary"
-                  type="button"
-                  onClick={handleSearch}
+          <Form>
+            <h1 style={{ fontFamily: "Karla" }}>GitStats</h1>
+            <h2>
+              Pesquise por perfis e acompanhe as estatísticas de seus usuários
+              e repositórios de maneira regular.
+            </h2>
+            <InputGroup
+              className={`busca ${theme}`}
+              style={{ border: "none", borderRadius: "15px" }}
+            >
+              <Form.Control
+                type="text"
+                className="input-name-user"
+                placeholder="Digite o nome do usuário"
+                onChange={handleUsernameSearch}
+                onKeyDown={handleKeyDown}
+                value={userName}
+              />
+              <Button variant="primary" type="button" onClick={handleSearch}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="#181719"
+                  className="bi bi-search"
+                  viewBox="0 0 16 16"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="#181719"
-                    className="bi bi-search"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                  </svg>
-                </Button>
-              </InputGroup>
-              <div className="link-container">
-                <Button
-                  variant="link"
-                  target="_blank"
-                  href={
-                    "https://github.com/signup?ref_cta=Sign+up&ref_loc=header+logged+out&ref_page=%2F&source=header-home"
-                  }
-                >
-                  Não tem conta no GitHub Cadastre-se!
-                </Button>
-              </div>
-            </Form>
-          </div>
-          <Modal show={showModal} onHide={handleCloseModal}>
-            <Modal.Header closeButton>
-              <Modal.Title>Usuário não encontrado</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              O nome de usuário informado não existe no Github. Por favor,
-              insira um nome de usuário válido.
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="dark" onClick={handleCloseModal}>
-                Fechar
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                </svg>
               </Button>
-            </Modal.Footer>
-          </Modal>
-        </Container>
+            </InputGroup>
+            <div className="link-container">
+              <Button
+                variant="link"
+                target="_blank"
+                href={
+                  "https://github.com/signup?ref_cta=Sign+up&ref_loc=header+logged+out&ref_page=%2F&source=header-home"
+                }
+              >
+                Não tem conta no GitHub? Cadastre-se!
+              </Button>
+            </div>
+          </Form>
+        </div>
+        <Modal show={showModal} onHide={handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Usuário não encontrado</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            O nome de usuário informado não existe no Github. Por favor,
+            insira um nome de usuário válido.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="dark" onClick={handleCloseModal}>
+              Fechar
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Container>
     </main>
   );
 }
